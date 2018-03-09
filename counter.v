7d08390f0a4f14f034e21a4536fd5fa4515dc60c
ayush210 <ayushmalpani21@gmail.com>
2018-03-09 22:26:00 +0530

    counter_module

---
+++ counter.v
@@ -0,0 +1,36 @@
+module counter(in,en,clk);
+input clk;
+input en;
+output[7:0] in;
+reg[7:0] in;
+always @(posedge clk)
+		begin
+		if(!en)
+		begin
+		in <= in + 1;
+		end
+		end	
+		initial begin
+		in = 0;
+		end
+endmodule
+
+
+
+module test;
+reg clk;
+wire[7:0] in;
+reg en;
+counter c(in,en,clk);
+always @(*)
+		begin
+#5 clk<= ~clk;
+		end
+		initial begin
+		$monitor("%d",in);
+		en = 0;clk = 0;
+#50 $finish;
+		end
+endmodule
+
+
